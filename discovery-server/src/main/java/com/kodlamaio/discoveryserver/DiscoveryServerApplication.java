5d8462ce08e2d0d300912908f21d2e86c06256b1
hamzapamuk23 <hamzapamuk@smartiys.com.tr>
2023-05-31 19:17:22 +0300

    feat: config server, maintenance server, payment server added

--- discovery-server/src/main/java/com/kodlamaio/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/kodlamaio/discoveryserver/DiscoveryServerApplication.java
@@ -2,15 +2,14 @@ package com.kodlamaio.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
 
 }
